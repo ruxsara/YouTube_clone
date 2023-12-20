fea93ef6803495fec4f7d1894f4ce66a8ac86ed5
ruxsara <87611734+ruxsara@users.noreply.github.com>
2023-12-20 23:51:52 +0400

    Feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Box} from '@mui/material'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+
+function App() {
+
+  return (
+  <Box>
+    
+  </Box>
+  )
+}
+
+export default App
