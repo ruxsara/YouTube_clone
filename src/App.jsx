65331b6b217f81d7de5059a7623d6fa7d7a5bcf3
ruxsara <87611734+ruxsara@users.noreply.github.com>
2023-12-23 01:07:00 +0400

    Fix: solved problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { Box } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Navbar,Feed,VideoDetail,ChannelDetail,SearchFeed} from './components/index'
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Box sx={{ backgroundColor: "#000" }}>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Feed />} />
-          <Route path="/video:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
-  );
-}
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
+
 export default App;
