0462325bc3ab0abf7b9945c7b731103bb4bcae66
Pashazade <nazar.pashazade@accenture.com>
2023-12-25 14:38:40 +0200

    update baseurl

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
+import { BASE } from './src/components/contains'
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     plugins: [react()],
-    base:'/youtube-clone/',
+    base:`/${BASE}/`,
     define: {
       __RAPID_API_KEY__: JSON.stringify(env.REACT_APP_RAPID_API_KEY),
       __RAPID_API_BASE_URL__:JSON.stringify(env.REACT_APP_RAPID_API_BASE_URL),
